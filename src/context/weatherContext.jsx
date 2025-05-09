2d7b6c161a5bcbd3a441dde09a53ebd77c613427
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-09 15:56:20 +0530

    Create Weather Context

---
+++ src/context/weatherContext.jsx
@@ -0,0 +1,12 @@
+import { React, createContext, useState } from "react";
+
+export const WeatherContext = createContext();
+
+export const WeatherProvider = ({ children }) => {
+  const [weather, setWeather] = useState(null);
+  return (
+    <WeatherContext.Provider value={{ weather, setWeather }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+};
