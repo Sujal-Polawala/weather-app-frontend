34fe640506b227f5ae12200457bfb110e4dddde4
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-09 17:21:51 +0530

    Update

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/weather';
-export const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:5000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://weather-app-backend-8yje.onrender.com/api/weather';
+export const API_BASE = import.meta.env.VITE_API_BASE || "https://weather-app-backend-8yje.onrender.com/";
