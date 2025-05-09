14d6ece5bfd8325e171aee3ea484f1c1630d5c05
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-09 15:54:52 +0530

    Add Base_URL

---
+++ src/config/apiConfig.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/weather';
+export const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:5000";
