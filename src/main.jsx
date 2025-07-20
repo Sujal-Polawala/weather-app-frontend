3c9b9df429c87b9827f446e76a799f0fd7be16f8
Sujal-Polawala <sujalpolawala@gmail.com>
2025-07-20 18:29:38 +0530

    Fix Some Bugs

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { React, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import 'react-select/dist/react-select.css';
 
 // export const OPENCAGE_API_KEY= process.env.OPENCAGE_API_KEY;
 
